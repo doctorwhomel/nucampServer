1cca8e89c9910d28239ffb12edf36d7454272f74
WHOMEL <williamhomel@gmail.com>
2022-11-21 12:17:16 -0800

    workshop3 assignment

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -210,20 +210,31 @@ campsiteRouter
     Campsite.findById(req.params.campsiteId)
       .then((campsite) => {
         if (campsite && campsite.comments.id(req.params.commentId)) {
-          if (req.body.rating) {
-            campsite.comments.id(req.params.commentId).rating = req.body.rating;
+          if (
+            campsite.comments
+              .id(req.params.commentId)
+              .author._id.equals(req.user._id)
+          ) {
+            if (req.body.rating) {
+              campsite.comments.id(req.params.commentId).rating =
+                req.body.rating;
+            }
+            if (req.body.text) {
+              campsite.comments.id(req.params.commentId).text = req.body.text;
+            }
+            campsite
+              .save()
+              .then((campsite) => {
+                res.statusCode = 200;
+                res.setHeader("Content-Type", "application/json");
+                res.json(campsite);
+              })
+              .catch((err) => next(err));
+          } else {
+            err = new Error("You are not authorized to update this comment!");
+            err.status = 403;
+            return next(err);
           }
-          if (req.body.text) {
-            campsite.comments.id(req.params.commentId).text = req.body.text;
-          }
-          campsite
-            .save()
-            .then((campsite) => {
-              res.statusCode = 200;
-              res.setHeader("Content-Type", "application/json");
-              res.json(campsite);
-            })
-            .catch((err) => next(err));
         } else if (!campsite) {
           err = new Error(`Campsite ${req.params.campsiteId} not found`);
           err.status = 404;
@@ -236,13 +247,15 @@ campsiteRouter
       })
       .catch((err) => next(err));
   })
-  .delete(
-    authenticate.verifyUser,
-    authenticate.verifyAdmin,
-    (req, res, next) => {
-      Campsite.findById(req.params.campsiteId)
-        .then((campsite) => {
-          if (campsite && campsite.comments.id(req.params.commentId)) {
+  .delete(authenticate.verifyUser, (req, res, next) => {
+    Campsite.findById(req.params.campsiteId)
+      .then((campsite) => {
+        if (campsite && campsite.comments.id(req.params.commentId)) {
+          if (
+            campsite.comments
+              .id(req.params.commentId)
+              .author._id.equals(req.user._id)
+          ) {
             campsite.comments.id(req.params.commentId).remove();
             campsite
               .save()
@@ -252,18 +265,22 @@ campsiteRouter
                 res.json(campsite);
               })
               .catch((err) => next(err));
-          } else if (!campsite) {
-            err = new Error(`Campsite ${req.params.campsiteId} not found`);
-            err.status = 404;
-            return next(err);
           } else {
-            err = new Error(`Comment ${req.params.commentId} not found`);
-            err.status = 404;
+            err = new Error("You are not authorized to delete this comment!");
+            err.status = 403;
             return next(err);
           }
-        })
-        .catch((err) => next(err));
-    }
-  );
+        } else if (!campsite) {
+          err = new Error(`Campsite ${req.params.campsiteId} not found`);
+          err.status = 404;
+          return next(err);
+        } else {
+          err = new Error(`Comment ${req.params.commentId} not found`);
+          err.status = 404;
+          return next(err);
+        }
+      })
+      .catch((err) => next(err));
+  });
 
 module.exports = campsiteRouter;
